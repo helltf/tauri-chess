73920a2ac6f1ff6e79b63473efe8361c20661eae
helltf <dario.zachow5530@gmail.com>
2022-10-16 14:34:41 +0200

    feat(game): get current position from rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,12 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
+mod game;
+use game::game::get_position;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn greet() -> String {
+    get_position()    
 }
 
 fn main() {
--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -1,11 +1,12 @@
-import {For} from "solid-js";
+import {createEffect, createSignal, For} from "solid-js";
+import {invoke} from '@tauri-apps/api/tauri'
 import Tile from "./Tile";
 import './Board.css'
 
 function getColor(index: number): 'black' | 'white' {
 	const row = Math.floor(index / 8)
 
-	let rowStartColor: 'black'|'white' = row % 2 ? 'white' : 'black'
+	let rowStartColor: 'black' | 'white' = row % 2 ? 'white' : 'black'
 
 	if (index % 2) {
 		rowStartColor = rowStartColor === 'white' ? 'black' : 'white'
@@ -13,6 +14,12 @@ function getColor(index: number): 'black' | 'white' {
 
 	return rowStartColor
 }
+
+const [position, setPosition] = createSignal('')
+
+createEffect(async () => {
+	setPosition(await invoke("greet"))
+}, [position])
 
 function Board() {
 	return (<div class="board">
