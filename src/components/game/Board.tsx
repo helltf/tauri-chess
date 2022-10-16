873b674a12f84320236feb11d5fce7fe273ad397
helltf <dario.zachow5530@gmail.com>
2022-10-16 12:15:43 +0200

    feat(board): display board with correct colors

--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -2,10 +2,22 @@ import {For} from "solid-js";
 import Tile from "./Tile";
 import './Board.css'
 
+function getColor(index: number): 'black' | 'white' {
+	const row = Math.floor(index / 8)
+
+	let rowStartColor: 'black'|'white' = row % 2 ? 'white' : 'black'
+
+	if (index % 2) {
+		rowStartColor = rowStartColor === 'white' ? 'black' : 'white'
+	}
+
+	return rowStartColor
+}
+
 function Board() {
 	return (<div class="board">
 		<For each={Array(64).fill("1")}>{(_, i) =>
-			<Tile />}
+			<Tile number={i()} color={getColor(i())} />}
 		</For>
 	</div>)
 }
--- src/components/game/Tile.tsx
+++ src/components/game/Tile.tsx
@@ -1,5 +1,7 @@
-function Tile() {
-	return <div>Tile</div>
+import './Tile.css'
+
+function Tile(props: {number: number, color: "black" | "white"}) {
+	return <div style="color: red;" class={`tile tile-${props.color}`}>{props.number}</div>
 }
 
 export default Tile
