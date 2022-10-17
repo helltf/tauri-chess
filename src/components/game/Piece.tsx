8d22135f45457a8b3e10aa95d5584abc884b0e85
helltf <dario.zachow5530@gmail.com>
2022-10-17 13:06:25 +0200

    fix(board): small styling changes

--- src/components/game/Piece.tsx
+++ src/components/game/Piece.tsx
@@ -1,8 +1,8 @@
-import {lazy} from "solid-js";
 import {Piece as PieceType} from "../../ts/fen/parser";
 import './Piece.css'
+
 function Piece(props: {xIndex: number, yIndex: number, piece: PieceType}) {
-	return (<div style={{left: `calc(100vh/8 * ${props.xIndex})`, top: `calc(100vh/8 * ${props.yIndex})`}} class="piece">
+	return (<div style={{left: `calc(100%/8 * ${props.xIndex})`, top: `calc(100%/8 * ${props.yIndex})`}} class="piece">
 		<img src={`../../../public/pieces/${props.piece.color}_${props.piece.type}.svg`}></img>
 	</div>)
 }
--- src/components/game/Tile.tsx
+++ src/components/game/Tile.tsx
@@ -1,7 +1,7 @@
 import './Tile.css'
 
 function Tile(props: {number: number, color: "black" | "white"}) {
-	return <div style="color: red;" class={`tile tile-${props.color}`}>{props.number}</div>
+	return <div  class={`tile tile-${props.color}`}>{props.number}</div>
 }
 
 export default Tile
