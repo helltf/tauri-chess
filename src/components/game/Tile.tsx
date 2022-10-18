836f328b2d2611085b395982f70109aed490d31c
helltf <dario.zachow5530@gmail.com>
2022-10-18 09:57:06 +0200

    fix(board): proper positioning of pieces

--- src/components/game/Tile.tsx
+++ src/components/game/Tile.tsx
@@ -2,9 +2,9 @@ import './Tile.css'
 import {Piece as PieceType} from '../../ts/fen/parser'
 import Piece from './Piece'
 
-function Tile(props: {piece: PieceType, number: number, color: "black" | "white"}) {
+function Tile(props: {piece: PieceType | null, number: number, color: "black" | "white"}) {
 	return <div data-number={props.number} class={`tile tile-${props.color}`}>
-<Piece piece={props.piece}/>
+		<Piece piece={props.piece} />
 	</div>
 }
 
