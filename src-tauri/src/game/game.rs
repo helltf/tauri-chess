668f70c870f6ce0cfba5a2296dd2edd176bdd5ed
helltf <dario.zachow5530@gmail.com>
2022-10-20 11:03:45 +0200

    feat(parser): add fen parser in backend

--- src-tauri/src/game/game.rs
+++ src-tauri/src/game/game.rs
@@ -2,14 +2,131 @@ pub fn get_position() -> String {
     return "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
 }
 
-pub struct Piece {}
+#[derive(Debug)]
+pub enum PieceType {
+    KING,
+    ROOK,
+    QUEEN,
+    KNIGHT,
+    PAWN,
+    BISHOP,
+}
+
+#[derive(Debug)]
+pub enum PieceColor {
+    WHITE,
+    BLACK,
+}
+#[derive(Debug)]
+pub struct Piece {
+    pub piece_type: PieceType,
+    pub color: PieceColor,
+}
 
 pub struct Game {
     pub board: Vec<Vec<Option<Piece>>>,
     pub white_move: bool,
 }
+
 impl Game {
-    pub fn New() -> Game {
-        Game{board:}
+    pub fn new() -> Game {
+        Game {
+            board: parse_fen(get_position()),
+            white_move: (true),
+        }
     }
+}
+
+fn is_lowercase(character: char) -> bool {
+    return char::is_lowercase(character);
+}
+
+fn get_piece(character: char) -> Option<Piece> {
+    let color: PieceColor = if is_lowercase(character) {
+        PieceColor::BLACK
+    } else {
+        PieceColor::WHITE
+    };
+
+    let low_char = String::from_iter(character.to_lowercase().collect::<Vec<_>>());
+
+    if low_char == "k" {
+        return Some(Piece {
+            piece_type: PieceType::KING,
+            color: color,
+        });
+    }
+
+    if low_char == "r" {
+        return Some(Piece {
+            piece_type: PieceType::ROOK,
+            color: color,
+        });
+    }
+
+    if low_char == "q" {
+        return Some(Piece {
+            piece_type: PieceType::QUEEN,
+            color: color,
+        });
+    }
+
+    if low_char == "n" {
+        return Some(Piece {
+            piece_type: PieceType::KNIGHT,
+            color: color,
+        });
+    }
+
+    if low_char == "p" {
+        return Some(Piece {
+            piece_type: PieceType::PAWN,
+            color: color,
+        });
+    }
+
+    if low_char == "b" {
+        return Some(Piece {
+            piece_type: PieceType::BISHOP,
+            color: color,
+        });
+    }
+
+    None
+}
+
+fn parse_row(row: &str) -> Vec<Option<Piece>> {
+    let chars = row.chars().collect::<Vec<char>>();
+    let mut result: Vec<Option<Piece>> = Vec::new();
+
+    for character in chars {
+        let piece = get_piece(character);
+        match piece {
+            Some(p) => result.push(Some(p)),
+            None => {
+                let offset = character as u32 - '0' as u32;
+                for i in 0..offset {
+                    result.push(None)
+                }
+            }
+        }
+    }
+    result
+}
+
+fn parse_position(position: &str) -> Vec<Vec<Option<Piece>>> {
+    let rows: Vec<&str> = position.split('/').collect();
+
+    let parsed_rows = rows
+        .iter()
+        .map(|row| parse_row(row))
+        .collect::<Vec<Vec<Option<Piece>>>>();
+    parsed_rows
+}
+
+fn parse_fen(fen: String) -> Vec<Vec<Option<Piece>>> {
+    let parts: Vec<&str> = fen.split(" ").collect::<Vec<&str>>();
+
+    let pieces = parse_position(parts[0]);
+    return pieces;
 }
--- src-tauri/src/handlers/commands.rs
+++ src-tauri/src/handlers/commands.rs
@@ -2,4 +2,7 @@ use crate::game::game::Game;
 use tauri::State;
 
 #[tauri::command]
-pub fn get_position(state: State<Game>) -> String {}
+pub fn get_position(state: State<Game>) -> String {
+println!("{:?}", state.board);
+    return "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
+}
