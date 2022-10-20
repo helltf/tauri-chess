6ab5fc402e184ac7ab98cbd7c79040d7bfcaab7c
helltf <dario.zachow5530@gmail.com>
2022-10-20 13:54:18 +0200

    feat: check king moves

--- src-tauri/src/game/action.rs
+++ src-tauri/src/game/action.rs
@@ -7,7 +7,18 @@ pub fn king_action(
     from_x: i32,
     from_y: i32,
 ) -> Result<String, String> {
-    Ok("success".to_string())
+    let x_diff = from_x - x;
+    let y_diff = from_y - y;
+    let y_valid = y_diff == -1 || y_diff == 1;
+    let x_valid = x_diff == -1 || x_diff == 1;
+    let x_no_diff = x_diff == 0;
+    let y_no_diff = y_diff == 0;
+
+    if (y_valid && x_no_diff) || (x_valid && y_no_diff) || (x_valid && y_valid) {
+        return Ok("success".to_string());
+    }
+
+    Err("Invalid move".to_string())
 }
 pub fn queen_action(
     piece: Piece,
