31948eeec1840a30efc0041a2c2b75a3117a92de
helltf <dario.zachow5530@gmail.com>
2022-10-20 09:38:54 +0200

    feat(game): create piece and game struct

--- src-tauri/src/game/game.rs
+++ src-tauri/src/game/game.rs
@@ -1,3 +1,15 @@
 pub fn get_position() -> String {
     return "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
 }
+
+pub struct Piece {}
+
+pub struct Game {
+    pub board: Vec<Vec<Option<Piece>>>,
+    pub white_move: bool,
+}
+impl Game {
+    pub fn New() -> Game {
+        Game{board:}
+    }
+}
