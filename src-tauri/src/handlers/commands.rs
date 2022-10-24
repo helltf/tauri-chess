35be384f9c7942fe1f66c89ee812da32376bbb4c
helltf <dario.zachow5530@gmail.com>
2022-10-24 10:19:05 +0200

    feat: check move is legal

--- src-tauri/src/handlers/commands.rs
+++ src-tauri/src/handlers/commands.rs
@@ -12,47 +12,46 @@ pub fn action(
     y: i32,
     from_x: i32,
     from_y: i32,
-    board: State<Board>,
+    mut board:State<Board>,
 ) -> Result<String, String> {
-    println!("{}{}", x, y);
     let to_square = get_square(x, y);
     let from_square = get_square(from_x, from_y);
-
     let action = ChessMove::new(from_square, to_square, None);
-    board.make_move_new(action);
+    let result = board.make_move(action, &board);
+    println!("{:?}", result);
     return Err("abc".to_string());
 }
 
 pub fn get_square(x: i32, y: i32) -> Square {
-    Square::make_square(get_rank(y).unwrap(), get_file(x).unwrap())
+    let rank = get_rank(y).expect("invalid rank");
+    let file = get_file(x).expect("invalid file");
+    Square::make_square(rank, file)
 }
 
 pub fn get_rank(rank: i32) -> Option<Rank> {
-    println!("rank: {}", rank);
     match rank {
-        1 => Some(Rank::First),
-        2 => Some(Rank::Second),
-        3 => Some(Rank::Third),
+        7 => Some(Rank::First),
+        6 => Some(Rank::Second),
+        5 => Some(Rank::Third),
         4 => Some(Rank::Fourth),
-        5 => Some(Rank::Fifth),
-        6 => Some(Rank::Sixth),
-        7 => Some(Rank::Seventh),
-        8 => Some(Rank::Eighth),
+        3 => Some(Rank::Fifth),
+        2 => Some(Rank::Sixth),
+        1 => Some(Rank::Seventh),
+        0 => Some(Rank::Eighth),
         _ => None,
     }
 }
 
 pub fn get_file(file: i32) -> Option<File> {
-    println!("file: {}", file);
     match file {
-        1 => Some(File::A),
-        2 => Some(File::B),
-        3 => Some(File::C),
+        7 => Some(File::A),
+        6 => Some(File::B),
+        5 => Some(File::C),
         4 => Some(File::D),
-        5 => Some(File::E),
-        6 => Some(File::F),
-        7 => Some(File::G),
-        8 => Some(File::H),
+        3 => Some(File::E),
+        2 => Some(File::F),
+        1 => Some(File::G),
+        0 => Some(File::H),
         _ => None,
     }
 }
