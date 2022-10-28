790851477d15cd8ad76099971ec74b7e71db9b29
helltf <dario.zachow5530@gmail.com>
2022-10-28 20:56:34 +0200

    feat: input field for inserting fen position

--- src/components/start/Start.tsx
+++ src/components/start/Start.tsx
@@ -1,9 +1,9 @@
 import {A, useNavigate} from '@solidjs/router'
-import {createSignal} from 'solid-js'
+import {createSignal, Signal} from 'solid-js'
 import './Start.css'
 
 function Start() {
-	const [position, setPosition] = createSignal(null)
+	const [position, setPosition]: Signal<string | null> = createSignal(null)
 	const navigate = useNavigate()
 
 	const startGame = () => {
@@ -13,6 +13,7 @@ function Start() {
 	return (
 		<main class='main-view'>
 			<h1 class='main-headline'>Chess</h1>
+			<input onInput={event => setPosition(event.data)} />
 			<button onclick={startGame} class='game-btn'>Start a Game</button>
 		</main>
 	)
