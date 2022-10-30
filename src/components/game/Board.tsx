8dc86da26dd7bd8befd68e1abbdfb8969b835a3c
helltf <dario.zachow5530@gmail.com>
2022-10-30 12:55:47 +0100

    feat: style input field

--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -1,77 +1,85 @@
-import Tile from './Tile'
-import './Board.css'
-import {createSignal, For} from 'solid-js'
-import {parse, Piece, Pieces} from '../../ts/fen/parser'
-import {invoke} from '@tauri-apps/api/tauri'
-import {onMount} from 'solid-js'
-import Reset from './Reset'
-import {useLocation} from '@solidjs/router'
+import Tile from "./Tile";
+import "./Board.css";
+import { createSignal, For } from "solid-js";
+import { parse, Piece, Pieces } from "../../ts/fen/parser";
+import { invoke } from "@tauri-apps/api/tauri";
+import { onMount } from "solid-js";
+import Reset from "./Reset";
+import { useLocation } from "@solidjs/router";
 
-type Color = 'black' | 'white'
+type Color = "black" | "white";
 
 function getColor(x: number, y: number): Color {
-	let rowStartColor: Color = y % 2 ? 'white' : 'black'
+  let rowStartColor: Color = y % 2 ? "white" : "black";
 
-	if (x % 2) {
-		rowStartColor = rowStartColor === 'white' ? 'black' : 'white'
-	}
+  if (x % 2) {
+    rowStartColor = rowStartColor === "white" ? "black" : "white";
+  }
 
-	return rowStartColor
+  return rowStartColor;
 }
 
 function Board() {
-	const [board, setBoard] = createSignal([[]] as Pieces)
-	const location = useLocation()
+  const [board, setBoard] = createSignal([[]] as Pieces);
+  const location = useLocation();
 
-	onMount(async () => {
-		const state = location.state as Readonly<Partial<{position: string | null}>>
+  onMount(async () => {
+    const state = location.state as Readonly<
+      Partial<{ position: string | null }>
+    >;
 
-		const position = state?.position ?? await invoke<string>('get_position')
-		await invoke('set_position', {position})
-		setBoard(parse(position))
-	})
+    const position = state?.position ?? (await invoke<string>("get_position"));
+    await invoke("set_position", { position });
+    setBoard(parse(position));
+  });
 
-	const onReset = async () => {
-		await invoke('reset')
-		const currentPosition = await invoke<string>('get_position')
-		setBoard(parse(currentPosition))
-	}
+  const onReset = async () => {
+    await invoke("reset");
+    const currentPosition = await invoke<string>("get_position");
+    setBoard(parse(currentPosition));
+  };
 
-	const onMove = async (piece: Piece, x: number, y: number, fromX: number, fromY: number) => {
-		try {
-			await invoke('action', {x, y, fromX, fromY})
-		} catch (e) {
-			return console.error(e)
-		}
-		const newBoard = JSON.parse(JSON.stringify(board()))
+  const onMove = async (
+    piece: Piece,
+    x: number,
+    y: number,
+    fromX: number,
+    fromY: number
+  ) => {
+    try {
+      await invoke("action", { x, y, fromX, fromY });
+    } catch (e) {
+      return console.error(e);
+    }
+    const newBoard = JSON.parse(JSON.stringify(board()));
 
-		newBoard[y][x] = piece
-		newBoard[fromY][fromX] = null
-		setBoard(newBoard)
-	}
+    newBoard[y][x] = piece;
+    newBoard[fromY][fromX] = null;
+    setBoard(newBoard);
+  };
 
-	return (
-		<div class='game'>
-			<div id='board' class='board'>
-				<For each={board()}>
-					{(row, rowIndex) => (
-						<For each={row}>
-							{(p, colIndex) => (
-								<Tile
-									onMove={onMove}
-									x={colIndex()}
-									y={rowIndex()}
-									color={getColor(colIndex(), rowIndex())}
-									piece={p}
-								/>
-							)}
-						</For>
-					)}
-				</For>
-			</div>
-			<Reset onReset={onReset} ></Reset>
-		</div >
-	)
+  return (
+    <div class="game">
+      <div id="board" class="board">
+        <For each={board()}>
+          {(row, rowIndex) => (
+            <For each={row}>
+              {(p, colIndex) => (
+                <Tile
+                  onMove={onMove}
+                  x={colIndex()}
+                  y={rowIndex()}
+                  color={getColor(colIndex(), rowIndex())}
+                  piece={p}
+                />
+              )}
+            </For>
+          )}
+        </For>
+      </div>
+      <Reset onReset={onReset}></Reset>
+    </div>
+  );
 }
 
-export default Board
+export default Board;
--- src/ts/fen/parser.ts
+++ src/ts/fen/parser.ts
@@ -1,75 +1,71 @@
 export enum PieceColor {
-	BLACK = 'BLACK',
-	WHITE = 'WHITE'
+  BLACK = "black",
+  WHITE = "white",
 }
 
 export enum PieceType {
-	KING = "KING",
-	QUEEN = "QUEEN",
-	ROOK = "ROOK",
-	KNIGHT = "KNIGHT",
-	PAWN = "PAWN",
-	BISHOP = "BISHOP"
+  KING = "king",
+  QUEEN = "queen",
+  ROOK = "rook",
+  KNIGHT = "knight",
+  PAWN = "pawn",
+  BISHOP = "bishop",
 }
 
 export interface Piece {
-	type: string
-	color: PieceColor
+  type: string;
+  color: PieceColor;
 }
 
-export type PieceRow = (Piece | null)[]
-export type Pieces = PieceRow[]
+export type PieceRow = (Piece | null)[];
+export type Pieces = PieceRow[];
 
-const pieceEncodings: {[key: string]: PieceType} = {
-	k: PieceType.KING,
-	r: PieceType.ROOK,
-	q: PieceType.QUEEN,
-	n: PieceType.KNIGHT,
-	p:PieceType.PAWN,
-	b: PieceType.BISHOP
-}
+const pieceEncodings: { [key: string]: PieceType } = {
+  k: PieceType.KING,
+  r: PieceType.ROOK,
+  q: PieceType.QUEEN,
+  n: PieceType.KNIGHT,
+  p: PieceType.PAWN,
+  b: PieceType.BISHOP,
+};
 
 const isLowerCase = (chars: string): boolean => {
-	return chars.toLowerCase() === chars
-}
+  return chars.toLowerCase() === chars;
+};
 
 const parseRow = (row: string): PieceRow => {
-	const chars = row.split('')
+  const chars = row.split("");
 
-	return chars.reduce((pieces, c) => {
-		const mappedPiece = pieceEncodings[c.toLowerCase()]
+  return chars.reduce((pieces, c) => {
+    const mappedPiece = pieceEncodings[c.toLowerCase()];
 
-		if (mappedPiece) {
-			pieces.push({
-				type: mappedPiece,
-				color: isLowerCase(c) ? PieceColor.BLACK : PieceColor.WHITE
-			})
-			return pieces
-		}
+    if (mappedPiece) {
+      pieces.push({
+        type: mappedPiece,
+        color: isLowerCase(c) ? PieceColor.BLACK : PieceColor.WHITE,
+      });
+      return pieces;
+    }
 
-		const offset = Number(c)
+    const offset = Number(c);
 
-		if (offset) {
-			pieces.push(...Array(offset).fill(null))
-		}
+    if (offset) {
+      pieces.push(...Array(offset).fill(null));
+    }
 
-		return pieces
-	}, [] as PieceRow)
-}
+    return pieces;
+  }, [] as PieceRow);
+};
 
 const parsePosition = (position: string): Pieces => {
-	const rows = position.split('/')
+  const rows = position.split("/");
 
-	return rows.map(parseRow)
-}
+  return rows.map(parseRow);
+};
 
 export const parse = (fen: string): Pieces => {
-	const [position,
-		move,
-		castling,
-		en_passant,
-		halfmove,
-		fullmove] = fen.split(' ')
-	const pieces = parsePosition(position)
-	return pieces
-}
+  const [position, move, castling, en_passant, halfmove, fullmove] =
+    fen.split(" ");
+  const pieces = parsePosition(position);
+  return pieces;
+};
