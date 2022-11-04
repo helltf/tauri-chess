441461a6ce8050578de99c3a60f2f3c9ffcf1e56
helltf <dario.zachow5530@gmail.com>
2022-11-04 10:35:28 +0100

    chore: remember current fen position in input

--- src/components/start/Start.tsx
+++ src/components/start/Start.tsx
@@ -3,32 +3,36 @@ import { useGame } from '../../context/gameContext'
 import './Start.css'
 
 function Start() {
-  const [gameContext, { setIsAi, setPosition }] = useGame() as any
+  const [gameContext, { setIsAi, setPosition }] = useGame()!
+
   return (
     <main class="main-view">
       <h1 class="main-headline">Chess</h1>
-      <input
-        class="fen-input"
-        placeholder="Start from position"
-        onInput={(e) => setPosition(e.currentTarget.value)}
-      />
-      <div class="game-type-select">
-        <button
-          onClick={(_) => setIsAi(false)}
-          class={`game-type-btn ${gameContext.isAi ? '' : 'selected'}`}
-        >
-          Player
-        </button>
-        <button
-          onClick={(_) => setIsAi(true)}
-          class={`game-type-btn ${gameContext.isAi ? 'selected' : ''}`}
-        >
-          AI
-        </button>
+      <div class="game-settings">
+        <input
+          class="fen-input"
+          placeholder="Start from position"
+          value={gameContext.position}
+          onInput={(e) => setPosition(e.currentTarget.value)}
+        />
+        <div class="game-type-select">
+          <button
+            onClick={(_) => setIsAi(false)}
+            class={`game-type-btn ${gameContext.isAi ? '' : 'selected'}`}
+          >
+            Player
+          </button>
+          <button
+            onClick={(_) => setIsAi(true)}
+            class={`game-type-btn ${gameContext.isAi ? 'selected' : ''}`}
+          >
+            AI
+          </button>
+        </div>
+        <A href="/game" class="game-btn">
+          Start a Game
+        </A>
       </div>
-      <A href="/game" class="game-btn">
-        Start a Game
-      </A>
     </main>
   )
 }
